adb2ae7e5a2e0a379846bfc10f9df3423893bc0b
Moutassim <Mdjodallah@gmail.com>
2025-03-20 08:25:21 +0100

    Modife next config

--- views/next.config.mjs
+++ views/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     reactStrictMode: true,
     compress: true,
     poweredByHeader: false,
+    images: {
+      domains: ['h-htransports.com'], // Ajoutez votre domaine ici
+    },
   };
   
   export default nextConfig;
